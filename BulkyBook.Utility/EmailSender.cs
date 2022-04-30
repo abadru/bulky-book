0778d844c239a012a411d6c7b92f2e450abd44ff
Abdul Badru <abdul.badru@xpntek.com>
2022-04-30 17:15:20 +0200

    remove uncessary comments

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -17,25 +17,7 @@ public class EmailSender: IEmailSender
     }
     public Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
-        /*
-        var emailToSend = new MimeMessage();
-        emailToSend.From.Add(MailboxAddress.Parse("abdul.badru@uem.ac.mz"));
-        emailToSend.To.Add(MailboxAddress.Parse(email));
-        emailToSend.Subject = subject;
-        emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) {Text = htmlMessage};
-        
-        //send email
-        using (var emailClient = new MailKit.Net.Smtp.SmtpClient())
-        {
-            emailClient.Connect("smpt.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-            emailClient.Authenticate("abdul.badru@xpntek.com", "1nsp1r0n@P");
-            emailClient.Send(emailToSend);
-            
-            emailClient.Disconnect(true);
-        }
-                return Task.CompletedTask;
-
-        */
+    
 
         var client = new SendGridClient(SendGridSecret);
         var from = new EmailAddress("abdul.badru@uem.ac.mz", "Bulky Book");
